9b23589741f73cb72d6ab91f31325fc02b7427a5
Oleg Shatov <8730199@gmail.com>
2022-05-03 23:55:20 +0300

    Main test file cleanup

--- tests/main.cpp
+++ tests/main.cpp
@@ -1,11 +1,2 @@
-#include <boost/ut.hpp>
-
 int main() {
 }
-
-//auto main(int argc, const char** argv) -> int {
-//  using namespace boost::ut;
-//  expect((argc == 2_i) >> fatal) << "Not enough parameters!";
-//  cfg<override> = {.filter = argv[1]};
-//  return cfg<override>.run();
-//}
