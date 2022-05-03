e20dd0f4e133e040c4c61c1857b5749bbe425f11
Oleg Shatov <8730199@gmail.com>
2022-05-03 09:46:27 +0300

    Refactor

--- dllib/autograd.hpp
+++ dllib/autograd.hpp
@@ -14,88 +14,97 @@ struct IArbitraryVariable;
 using TArbitraryVariable = std::shared_ptr<IArbitraryVariable>;
 
 struct IArbitraryVariable {
-    // NOLINTNEXTLINE
-    IArbitraryVariable(bool requires_grad) : requires_grad(requires_grad) {}
+  // NOLINTNEXTLINE
+  IArbitraryVariable(bool requires_grad) : requires_grad(requires_grad) {}
 
-    [[nodiscard]] virtual std::vector<TArbitraryVariable> GetChildren() const = 0;
-    virtual void PushGradient() = 0;
-    virtual ~IArbitraryVariable() = default;
+  [[nodiscard]] virtual std::vector<TArbitraryVariable> GetChildren() const = 0;
 
-    bool requires_grad;
+  virtual void PushGradient() = 0;
+
+  virtual ~IArbitraryVariable() = default;
+
+  bool requires_grad;
 };
 
 template<CTensor T>
 struct IVariable;
 
 template<CTensor T>
-using TVariable = std::shared_ptr<IVariable<T>>;
+class TVariable : public std::shared_ptr<IVariable<T>> {
+ public:
+  using std::shared_ptr<IVariable<T>>::shared_ptr;
+
+  // NOLINTNEXTLINE
+  TVariable(T value) : TVariable(std::make_shared<IVariable<T>>(value)) {
+  }
+};
 
 
 template<CTensor T>
 struct IVariable : public IArbitraryVariable {
-    using IArbitraryVariable::requires_grad;
+  using IArbitraryVariable::requires_grad;
 
-    // NOLINTNEXTLINE
-    IVariable(const T& value, bool requires_grad = false) : IArbitraryVariable(requires_grad), value(value) {}
+  // NOLINTNEXTLINE
+  IVariable(const T& value, bool requires_grad = false) : IArbitraryVariable(requires_grad), value(value) {}
 
-    ~IVariable() override = default;
+  ~IVariable() override = default;
 
-    template<class U = T>
-    std::enable_if_t<U::DimensionCount == 0, void> Backward() {
-        std::unordered_set<TArbitraryVariable> SubGraph;
-        std::vector<TArbitraryVariable> order;
+  template<class U = T>
+  std::enable_if_t<U::DimensionCount == 0, void> Backward() {
+    std::unordered_set<TArbitraryVariable> SubGraph;
+    std::vector<TArbitraryVariable> order;
 
-        auto dfs = [&SubGraph, &order](auto& self, TArbitraryVariable v) -> void {
-            SubGraph.insert(v);
-            for (auto&& child : v->GetChildren()) {
-                if (child->requires_grad && !SubGraph.count(child)) {
-                    self(self, std::move(child));
-                }
-            }
-            order.emplace_back(std::move(v));
-        };
-
-        for (auto&& child : GetChildren()) {
-            if (child->requires_grad && !SubGraph.contains(child)) {
-                dfs(dfs, std::move(child));
-            }
+    auto dfs = [&SubGraph, &order](auto& self, TArbitraryVariable v) -> void {
+      SubGraph.insert(v);
+      for (auto&& child: v->GetChildren()) {
+        if (child->requires_grad && !SubGraph.count(child)) {
+          self(self, std::move(child));
         }
+      }
+      order.emplace_back(std::move(v));
+    };
 
-        grad = 1;
-        PushGradient();
-        while (!order.empty()) {
-            order.back()->PushGradient();
-            order.pop_back();
-        }
+    for (auto&& child: GetChildren()) {
+      if (child->requires_grad && !SubGraph.contains(child)) {
+        dfs(dfs, std::move(child));
+      }
     }
 
-    void zero_grad() {
-        grad.FillWith(0);
+    grad = 1;
+    PushGradient();
+    while (!order.empty()) {
+      order.back()->PushGradient();
+      order.pop_back();
     }
+  }
 
-    T value;
-    T grad;
+  void zero_grad() {
+    grad.FillWith(0);
+  }
+
+  T value;
+  T grad;
 };
 
 
 template<CTensor T>
 struct TLeafNode final : public IVariable<T> {
-    using IVariable<T>::IVariable;
-    using IVariable<T>::value;
-    using IVariable<T>::grad;
-    using IVariable<T>::requires_grad;
-    using IVariable<T>::zero_grad;
+  using IVariable<T>::IVariable;
+  using IVariable<T>::value;
+  using IVariable<T>::grad;
+  using IVariable<T>::requires_grad;
+  using IVariable<T>::zero_grad;
 
-    [[nodiscard]] std::vector<TArbitraryVariable> GetChildren() const {
-        return {};
-    }
+  [[nodiscard]] std::vector<TArbitraryVariable> GetChildren() const {
+    return {};
+  }
 
-    void PushGradient() {}
+  void PushGradient() {}
 };
 
 template<CTensor T>
 inline constexpr TVariable<T> MakeLeaf(const T& value, bool requires_grad = false) {
-    return std::make_shared<TLeafNode<T>>(value, requires_grad);
+  return std::make_shared<TLeafNode<T>>(value, requires_grad);
 }
 
 
@@ -103,20 +112,20 @@ namespace helpers {
 
 template<class TFirstArg, class... TArgs>
 constexpr bool CalculateOr(TFirstArg first_arg, TArgs... other_args) {
-    if constexpr (sizeof...(TArgs) == 0) {
-        return first_arg;
-    } else {
-        return first_arg || CalculateOr(other_args...);
-    }
+  if constexpr (sizeof...(TArgs) == 0) {
+    return first_arg;
+  } else {
+    return first_arg || CalculateOr(other_args...);
+  }
 }
 
 template<CTensor T>
 constexpr std::optional<T*> GetGradientPointerIfRequired(const TVariable<T>& v) {
-    if (v->requires_grad) {
-        return &(v->grad);
-    } else {
-        return {};
-    }
+  if (v->requires_grad) {
+    return &(v->grad);
+  } else {
+    return {};
+  }
 }
 
 }  // namespace helpers
@@ -124,127 +133,128 @@ constexpr std::optional<T*> GetGradientPointerIfRequired(const TVariable<T>& v) 
 
 template<class TOperation, CTensor... TArgs>
 struct TOperationNode : public IVariable<std::invoke_result_t<decltype(&TOperation::Forward), TOperation*, TArgs...>> {
-    using TValue = std::invoke_result_t<decltype(&TOperation::Forward), TOperation*, TArgs...>;
+  using TValue = std::invoke_result_t<decltype(&TOperation::Forward), TOperation*, TArgs...>;
 
-    using IVariable<TValue>::IVariable;
-    using IVariable<TValue>::value;
-    using IVariable<TValue>::grad;
-    using IVariable<TValue>::requires_grad;
-    using IVariable<TValue>::zero_grad;
+  using IVariable<TValue>::IVariable;
+  using IVariable<TValue>::value;
+  using IVariable<TValue>::grad;
+  using IVariable<TValue>::requires_grad;
+  using IVariable<TValue>::zero_grad;
 
-    // NOLINTNEXTLINE
-    TOperationNode(TOperation op, const TVariable<TArgs>&... args) :
-        IVariable<TValue>(op.Forward(args->value...), helpers::CalculateOr(args->requires_grad...)),
-        operation_(std::move(op)),
-        args_({args...}) {}
+  // NOLINTNEXTLINE
+  TOperationNode(TOperation op, const TVariable<TArgs>& ... args) :
+    IVariable<TValue>(op.Forward(args->value...), helpers::CalculateOr(args->requires_grad...)),
+    operation_(std::move(op)),
+    args_({args...}) {}
 
-    TOperationNode(TOperationNode&&) noexcept = default;
+  TOperationNode(TOperationNode&&) noexcept = default;
 
-    [[nodiscard]] std::vector<TArbitraryVariable> GetChildren() const {
-        return [this]<size_t... i>(std::index_sequence<i...>) -> std::vector<TArbitraryVariable> {
-            return {static_pointer_cast<IArbitraryVariable>(get<i>(args_))...};
-        }(std::make_index_sequence<sizeof...(TArgs)>());
+  [[nodiscard]] std::vector<TArbitraryVariable> GetChildren() const {
+    return [this]<size_t... i>(std::index_sequence<i...>) -> std::vector<TArbitraryVariable>{
+      return { static_pointer_cast<IArbitraryVariable>(get<i>(args_))... };
+    }(std::make_index_sequence<sizeof...(TArgs)>());
+  }
+
+  void PushGradient() {
+    [this]<size_t... i>(std::index_sequence<i...>) {
+      operation_.Backward(grad, helpers::GetGradientPointerIfRequired(get<i>(args_))...);
     }
+    (std::make_index_sequence<sizeof...(TArgs)>());
+    zero_grad();
+  }
 
-    void PushGradient() {
-        [this]<size_t... i>(std::index_sequence<i...>) {
-            operation_.Backward(grad, helpers::GetGradientPointerIfRequired(get<i>(args_))...);
-        }(std::make_index_sequence<sizeof...(TArgs)>());
-        zero_grad();
-    }
-
-private:
-    TOperation operation_;
-    std::tuple<TVariable<TArgs>...> args_;
+ private:
+  TOperation operation_;
+  std::tuple<TVariable<TArgs>...> args_;
 };
 
 template<CTensor T>
 TVariable<T> operator+(const TVariable<T>& l, const TVariable<T>& r) {
-    struct TAddition {
-        T Forward(const T& l, const T& r) {
-            return l + r;
-        }
+  struct TAddition {
+    T Forward(const T& l, const T& r) {
+      return l + r;
+    }
 
-        void Backward(const T& grad, std::optional<T*> l, std::optional<T*> r) {
-            if (l) {
-                **l += grad;
-            }
-            if (r) {
-                **r += grad;
-            }
-        }
-    };
+    void Backward(const T& grad, std::optional<T*> l, std::optional<T*> r) {
+      if (l) {
+        **l += grad;
+      }
+      if (r) {
+        **r += grad;
+      }
+    }
+  };
 
-    return std::make_shared<TOperationNode<TAddition, T, T>>(TAddition{}, l, r);
+  return std::make_shared<TOperationNode<TAddition, T, T>>(TAddition{}, l, r);
 }
 
 template<CTensor T>
 TVariable<T> operator-(const TVariable<T>& l, const TVariable<T>& r) {
-    struct TSubtraction {
-        T Forward(const T& l, const T& r) {
-            return l - r;
-        }
+  struct TSubtraction {
+    T Forward(const T& l, const T& r) {
+      return l - r;
+    }
 
-        void Backward(const T& grad, std::optional<T*> l, std::optional<T*> r) {
-            if (l) {
-                **l += grad;
-            }
-            if (r) {
-                **r -= grad;
-            }
-        }
-    };
+    void Backward(const T& grad, std::optional<T*> l, std::optional<T*> r) {
+      if (l) {
+        **l += grad;
+      }
+      if (r) {
+        **r -= grad;
+      }
+    }
+  };
 
-    return std::make_shared<TOperationNode<TSubtraction, T, T>>(TSubtraction{}, l, r);
+  return std::make_shared<TOperationNode<TSubtraction, T, T>>(TSubtraction{}, l, r);
 }
 
 template<CTensor T>
 TVariable<T> operator*(const TVariable<T>& l, const TVariable<T>& r) {
-    struct TMultiplication {
-        T Forward(const T& l, const T& r) {
-            l_ = l;
-            r_ = r;
-            return l * r;
-        }
+  struct TMultiplication {
+    T Forward(const T& l, const T& r) {
+      l_ = l;
+      r_ = r;
+      return l * r;
+    }
 
-        void Backward(const T& grad, std::optional<T*> l, std::optional<T*> r) {
-            if (l) {
-                **l += grad * r_;
-            }
-            if (r) {
-                **r = l_ * grad;
-            }
-        }
+    void Backward(const T& grad, std::optional<T*> l, std::optional<T*> r) {
+      if (l) {
+        **l += grad * r_;
+      }
+      if (r) {
+        **r = l_ * grad;
+      }
+    }
 
-        T l_, r_;
-    };
+    T l_, r_;
+  };
 
-    return std::make_shared<TOperationNode<TMultiplication, T, T>>(TMultiplication{}, l, r);
+  return std::make_shared<TOperationNode<TMultiplication, T, T>>(TMultiplication{}, l, r);
 }
 
 template<CTensor T1, CTensor T2>
-auto MatrixMultiplication(const T1& l, const T2& r) {
-    struct TMatrixMultiplication {
-        helpers::MatrixMultiplicationResult<T1, T2> Forward(const T1& l, const T2& r) {
-            l_ = l;
-            r_ = r;
-            return MatrixMultiplication(l, r);
-        }
+auto MatrixMultiplication(const IVariable<T1>& l, const IVariable<T2>& r) {
+  struct TMatrixMultiplication {
+    helpers::MatrixMultiplicationResult<T1, T2> Forward(const T1& l, const T2& r) {
+      l_ = l;
+      r_ = r;
+      return MatrixMultiplication(l, r);
+    }
 
-        void Backward(const auto& grad, std::optional<T1*> l, std::optional<T2*> r) {
-            if (l) {
-                MatrixMultiplication(grad, r_.T(), **l);
-            }
-            if (r) {
-                MatrixMultiplication(l_.T(), grad, **r);
-            }
-        };
-
-        T1 l_;
-        T2 r_;
+    void Backward(const auto& grad, std::optional<T1*> l, std::optional<T2*> r) {
+      if (l) {
+        MatrixMultiplication(grad, r_.T(), **l);
+      }
+      if (r) {
+        MatrixMultiplication(l_.T(), grad, **r);
+      }
     };
 
-    return std::make_shared<TOperationNode<TMatrixMultiplication, T1, T2>>(TMatrixMultiplication{}, l, r);
+    T1 l_;
+    T2 r_;
+  };
+
+  return std::make_shared<TOperationNode<TMatrixMultiplication, T1, T2>>(TMatrixMultiplication{}, l, r);
 }
 
 // template<CTensor T>
