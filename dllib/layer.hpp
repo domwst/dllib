c5190cb7e7ce92c745d28872d1bdd7a64dcb8ced
Oleg Shatov <8730199@gmail.com>
2022-06-04 13:33:51 +0300

    Added bias to FullyConnected layer

--- dllib/layer.hpp
+++ dllib/layer.hpp
@@ -7,6 +7,50 @@
 
 namespace dllib {
 
+namespace helpers {
+
+template<class TData, size_t BatchSize, size_t... OtherDims>
+TTensor<TData, BatchSize, OtherDims...> AddBias(
+  const TTensor<TData, BatchSize, OtherDims...>& t,
+  const TTensor<TData, OtherDims...>& bias) {
+
+  auto result = t;
+  for (auto& x : result) {
+    x += bias;
+  }
+  return result;
+}
+
+template<class TData, size_t BatchSize, size_t... OtherDims>
+TVariable<TTensor<TData, BatchSize, OtherDims...>> AddBias(
+  const TVariable<TTensor<TData, BatchSize, OtherDims...>>& t,
+  const TVariable<TTensor<TData, OtherDims...>>& bias) {
+
+  using T = TTensor<TData, BatchSize, OtherDims...>;
+  using TBias = TTensor<TData, OtherDims...>;
+
+  struct TAddBias {
+    auto Forward(const T& t, const TBias& bias) {
+      return AddBias(t, bias);
+    }
+
+    void Backward(const T& grad, T* t, TBias* bias) {
+      if (t) {
+        *t += grad;
+      }
+      if (bias) {
+        for (auto& x : grad) {
+          *bias += x;
+        }
+      }
+    }
+  };
+
+  return std::make_shared<TOperationNode<TAddBias, T, TBias>>(TAddBias{}, t, bias);
+}
+
+}  // namespace helpers
+
 template<class TData, size_t From, size_t To>
 class FullyConnected {
  public:
@@ -19,28 +63,36 @@ class FullyConnected {
 
   template<class TGen>
   explicit FullyConnected(TGen gen) {
-    auto& data = var->value.template View<From * To>();
-    std::generate(data.Begin(), data.End(), std::move(gen));
+    for (auto& x : var->value.template View<-1u>()) {
+      x = gen();
+    }
+    for (auto& x : bias->value.template View<-1u>()) {
+      x = gen();
+    }
   }
 
   auto operator()(const auto& value) {
     if constexpr (VIsTensor<decltype(value)>) {
-      return MatrixProduct(value, var->value);
+      auto result = MatrixProduct(value, var->value);
+      return helpers::AddBias(result, bias->value);
     } else {
-      return MatrixProduct(value, var);
+      auto result = MatrixProduct(value, var);
+      return helpers::AddBias(result, bias);
     }
   }
 
-  std::tuple<TVariable<TTensor<TData, From, To>>&> GetParameters() {
-    return {var};
+  std::tuple<TVariable<TTensor<TData, From, To>>&, TVariable<TTensor<TData, To>>&> GetParameters() {
+    return {var, bias};
   }
 
-  std::tuple<const TVariable<TTensor<TData, From, To>>&> GetSerializationFields() const {
-    return {var};
+  std::tuple<const TVariable<TTensor<TData, From, To>>&, const TVariable<TTensor<TData, To>>&>
+  GetSerializationFields() const {
+    return {var, bias};
   }
 
  private:
   TVariable<TTensor<TData, From, To>> var{true};
+  TVariable<TTensor<TData, To>> bias{true};
 };
 
 }  // namespace dllib
