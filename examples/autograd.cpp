06bfe5f64cda6fdf4801ae4ca5ad7ca72868619e
Oleg Shatov <8730199@gmail.com>
2022-06-05 23:19:27 +0300

    Implemented autograd benchmark

--- examples/autograd.cpp
+++ examples/autograd.cpp
@@ -1,5 +1,8 @@
-#include <iostream>
+#include <dllib/tensor.hpp>
 #include <dllib/autograd.hpp>
+
+#include <iostream>
+#include <random>
 
 void LogExample() {
   dllib::TVariable<dllib::TTensor<float, 2, 2>> var({{1, 2},
@@ -19,7 +22,52 @@ void SqrtExample() {
   std::cout << var->grad << std::endl;
 }
 
+size_t FibonacciBenchmark() {
+  using TDouble = float;
+  using namespace dllib;
+
+  constexpr size_t N = 100, M = 100, K = 10'000;
+  std::array<TVariable<TTensor<TDouble, N, N>>, M> arr;
+
+  std::mt19937 rnd(std::random_device{}());
+  auto gen = [
+    &rnd,
+    distribution = std::normal_distribution<TDouble>{}]() mutable {
+
+    return distribution(rnd);
+  };
+
+  for (auto& var : arr) {
+    for (auto& x: var->value.View<-1u>()) {
+      x = gen();
+    }
+  }
+
+  auto start = clock();
+  for (size_t i = 0; i < K; ++i) {
+    size_t a = rnd() % M, b = rnd() % M;
+    if (rnd() % 2 == 0) {
+      arr[a] = arr[a] + arr[b];
+    } else {
+      arr[a] = arr[b] - arr[a];
+    }
+  }
+  auto sm = arr[0];
+  for (size_t i = 1; i < M; ++i) {
+    sm = sm + arr[i];
+  }
+  Sum(sm)->Backward();
+  auto stop = clock();
+
+  return (stop - start) * 1000 / CLOCKS_PER_SEC;
+}
+
 int main() {
-  LogExample();
+//  LogExample();
 //  SqrtExample();
+  size_t total_ms = 0;
+  for (size_t i = 0; i < 20; ++i) {
+    total_ms += FibonacciBenchmark();
+  }
+  std::cout << total_ms / 20 << std::endl;
 }
